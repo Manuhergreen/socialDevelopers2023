11ee3022cf5db3a15e7b4eebf2eae015f7146ed1
Manuhergreen <manuhernandezbi@gmail.com>
2023-07-06 15:19:33 +0200

    area personal

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoProyectosComponent } from './pages/listado-proyectos/listado-proyectos.component';
@@ -6,6 +6,8 @@ import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { AreaPersonalComponent } from './pages/area-personal/area-personal.component';
 import { RegisterComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PerfilUsuarioComponent } from './pages/area-personal/perfil-usuario/perfil-usuario.component';
+import { SubidaProyectosComponent } from './pages/area-personal/subida-proyectos/subida-proyectos.component';
 
 const routes: Routes = [
   {path: "",component:HomeComponent },
@@ -14,6 +16,8 @@ const routes: Routes = [
   {path: "areaPersonal",component:AreaPersonalComponent },
   {path: "register",component:RegisterComponent },
   {path: "login",component:LoginComponent },
+  {path: "perfil", component: PerfilUsuarioComponent},
+  {path: "subidaProyectos", component:SubidaProyectosComponent}
 
 ];
 
